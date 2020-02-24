130cd5d2dbbc253e5d09f90dd44d6c7feff67183
Lee Moriya <leecassie12@hotmail.co.uk>
2020-02-24 18:35:25 +0000

    PublicityStunt requirement removed! (Thanks Bee) Small cleanup of cs files

--- Downpour.cs
+++ Downpour.cs
@@ -7,9 +7,30 @@ using UnityEngine;
 using OptionalUI;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Security;
+using System.Runtime.CompilerServices;
 using System.IO;
 using RWCustom;
 using Menu;
+using System.Security.Permissions;
+
+[assembly: IgnoresAccessChecksTo("Assembly-CSharp")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[module: UnverifiableCode]
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class IgnoresAccessChecksToAttribute : Attribute
+    {
+        public IgnoresAccessChecksToAttribute(string assemblyName)
+        {
+            AssemblyName = assemblyName;
+        }
+
+        public string AssemblyName { get; }
+    }
+}
 
 public class RainScript : MonoBehaviour
 {
