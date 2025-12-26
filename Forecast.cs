8698650ca189b2e26e647453ec11e9f1cc963000
LeeMoriya <leecassie12@gmail.com>
2025-12-26 03:08:38 +0000

    v1.2.1

--- Forecast.cs
+++ Forecast.cs
@@ -18,11 +18,11 @@ using BepInEx;
 [module: UnverifiableCode]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 
-[BepInPlugin("LeeMoriya.Forecast", "Forecast", "1.2")]
+[BepInPlugin("LeeMoriya.Forecast", "Forecast", "1.2.1")]
 public class ForecastMod : BaseUnityPlugin
 {
     public static bool init = false;
-    public static string versionNum = "1.2";
+    public static string versionNum = "1.2.1";
     public ForecastMod()
     {
         
